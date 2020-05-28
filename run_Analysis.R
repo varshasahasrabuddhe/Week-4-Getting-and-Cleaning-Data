1b6c92dc9c365e96f12cb732da2b49679f57d431
varshasahasrabuddhe <63263429+varshasahasrabuddhe@users.noreply.github.com>
2020-05-28 13:55:59 -0700

    Add files via upload

--- run_Analysis.R
+++ run_Analysis.R
@@ -17,8 +17,8 @@ names(y_test) <- c("activity_code")
 
 obs_test <- read.table("x_test.txt")
 names(obs_test) <- as.character(features$V2)
+x_test<- obs_test[,grep("mean|Mean|std", names(obs_test))]
 
-x_test <- obs_test[,c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,227:228,240:241,253:254,266:271,294:296,345:350,373:375,424:429,452:454,503:504,513,516:517,526,529:530,539,542:543,552,555:561)]
 test <- cbind(s_test, y_test,x_test)
 
 s_train <- read.table("subject_train.txt")
@@ -33,11 +33,12 @@ names(y_train) <- c("activity_code")
 
 obs_train <- read.table("x_train.txt")
 names(obs_train) <- as.character(features$V2)
-x_train <- obs_train[,c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,227:228,240:241,253:254,266:271,294:296,345:350,373:375,424:429,452:454,503:504,513,516:517,526,529:530,539,542:543,552,555:561)]
+x_train<- obs_rain[,grep("mean|Mean|std", names(obs_test))]
+##x_train <- obs_train[,c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,227:228,240:241,253:254,266:271,294:296,345:350,373:375,424:429,452:454,503:504,513,516:517,526,529:530,539,542:543,552,555:561)]
 train <- cbind(s_train, y_train,x_train)
 
 Fulldata <- rbind(test,train)
-FinalData <- merge (Fulldata,activity_labels,by.x="activity_code", by.y="activity_code")
+##FinalData <- merge (Fulldata,activity_labels,by.x="activity_code", by.y="activity_code")
 
 FinalData <- merge (activity_labels,Fulldata,by.x="activity_code", by.y="activity_code")
 FinalData_1 <- FinalData%>%select(activity_desc,subject_id, 6:91)
